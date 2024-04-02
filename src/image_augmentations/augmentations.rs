25bf5351ec2d239234c0913e9b3b0a5c79df9d08
heabeounMKTO <heabeoun69zin@gmail.com>
2024-04-02 11:20:23 +0700

    brightness

--- src/image_augmentations/augmentations.rs
+++ src/image_augmentations/augmentations.rs
@@ -3,13 +3,14 @@ use crate::label::{YoloAnnotation, Xyxy, CoordinateType, LabelmeAnnotation};
 use anyhow::{Error, Result};
 use clap::Subcommand;
 use image::{self, DynamicImage, imageops, GenericImageView};
+use sorted_list::Tuples;
 use crate::fileutils::{write_yolo_to_txt, write_labelme_to_json};
 use crate::label::Shape;
 use uuid::Uuid;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use ndarray::prelude::*;
-
+use rand::prelude::*;
 
 #[derive(Debug)]
 pub enum AugmentationType {
@@ -50,17 +51,25 @@ impl ImageAugmentation {
     pub fn new(image: DynamicImage, coords: LabelmeAnnotation) -> ImageAugmentation {
          ImageAugmentation { image:  image, coords: coords }        
     }
+    
+    /// adds random amount of brightness in a given range 
+    /// negative values subtract brightness
+    pub fn random_brightness(&mut self, range: (i32, i32)) {
+        let mut rng = rand::thread_rng();
+        let p = imageops::brighten(&self.image, rng.gen_range(range.0..range.1));
+        self.image = DynamicImage::ImageRgba8(p);
+    }
 
     /// flips an image and it's annotation 
-    /// horizontally, or "along the y axis ☝️🤓" 
+    /// vertically , or "along the y axis ☝️🤓" 
     /// for u nerds out there
+    /// subtract y coordinates by image height then 
+    /// multiplies the coords by `[[1 , -1], [1 , -1]]`
+    /// to flip along the y axis
     pub fn flip_v(&mut self) {
        let flipped_v_image = imageops::flip_vertical(&self.image); 
        self.image = DynamicImage::ImageRgba8(flipped_v_image);
        for shape in self.coords.shapes.iter_mut() {
-            // subtract y coordinates by image height then 
-            // multiplies the coords by [[1 , -1], [1 , -1]]
-            // to flip along the y axis
             
             // subtract y coord by height and mult by -1
             shape.points[0][1] = ( shape.points[0][1] - (self.image.dimensions().1 as f32)) * -1.0; 
@@ -71,17 +80,18 @@ impl ImageAugmentation {
     }
 
     /// flips an image and it's annotation 
-    /// vertically , or "along the y axis ☝️🤓" 
+    /// horizontally , or "along the x axis ☝️🤓" 
     /// for u nerds out there
+    /// subtract y coordinates by image height then 
+    /// multiplies the coords by [[-1 , 1], [-1 , 1]]
+    /// to flip along the x axis
     pub fn flip_h(&mut self) {
        let flipped_h_image = imageops::flip_horizontal(&self.image); 
        self.image = DynamicImage::ImageRgba8(flipped_h_image);
        for shape in self.coords.shapes.iter_mut() {
-            // subtract y coordinates by image height then 
-            // multiplies the coords by [[-1 , 1], [-1 , 1]]
-            // to flip along the x axis
             
             // subtract x coord by width and mult by -1
+            // we dont use ndarrays here sir
             shape.points[0][0] = ( shape.points[0][0] - (self.image.dimensions().0 as f32)) * -1.0; 
             shape.points[1][0] = ( shape.points[1][0] - (self.image.dimensions().0 as f32)) * -1.0;
             println!("shape: {:?}", &shape);
