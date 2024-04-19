0d4d5700b7f66e05187ad3b890ecff64800a0c4b
heabeounMKTO <heabeoun69zin@gmail.com>
2024-04-19 17:58:53 +0700

    i fucking hate this IValue bullshit

--- examples/tch_inference.rs
+++ examples/tch_inference.rs
@@ -1,9 +1,18 @@
 extern crate kesa;
 
-
-use kesa::backends::tch_backend;
+use kesa::backends::tch_backend::{self, TchModel};
+fn load_tch(input: &str, device: Option<tch::Device>) -> TchModel {
+    let cuda = device.unwrap_or(tch::Device::cuda_if_available());
+    let loaded_model = TchModel::new(
+        &input, 640, 640, cuda
+    );
+    for n in 0..3 {
+        loaded_model.warmup().unwrap();
+    }
+    loaded_model
+}
 
 
 pub fn main() {
-    println!("AYEAS");
+    load_tch("test/cardDetv1.556_640x640.torchscript", None);  
 }
--- src/backends/tch_backend.rs
+++ src/backends/tch_backend.rs
@@ -1,21 +1,20 @@
-use tch::{self, vision::image};
-use tch::Tensor;
+use anyhow::{Error, Result};
+use std::io;
+use std::result;
 use tch::kind;
 use tch::IValue;
-use std::io;
-use anyhow::{Result, Error};
-
+use tch::Tensor;
+use tch::{self, vision::image};
 use crate::label::YoloAnnotation;
-
+use anyhow::{bail};
 pub struct TchModel {
     model: tch::CModule,
     device: tch::Device,
     w: i64,
-    h: i64 
+    h: i64,
 }
 
-
-impl TchModel{
+impl TchModel {
     pub fn new(weights: &str, w: i64, h: i64, device: tch::Device) -> TchModel {
         let mut model = tch::CModule::load_on_device(weights, device).unwrap();
         model.set_eval();
@@ -23,25 +22,34 @@ impl TchModel{
             model: model,
             device: device,
             w: w,
-            h: h
+            h: h,
         }
     }
 
-
-    pub fn run(&self, image: &tch::Tensor, 
-                conf_thresh: f32, iou_thresh: f64) 
-    -> Result<(), Error>    {
-        let mut img = tch::vision::image::resize(&image, self.w, self.h)?;
-        img = img.unsqueeze(0)
+    pub fn warmup(&self) -> Result<(), Error> {
+        let mut img: Tensor = Tensor::zeros(&[3, 640, 640], kind::INT64_CUDA);
+        img = img
+            .unsqueeze(0)
             .to_kind(tch::Kind::Float)
             .to_device(self.device)
             .g_div_scalar(255.0);
-        let pred = self.model
-            .forward_is(&[IValue::from(img)])
-            .unwrap();
+        let pred = self.model.forward_is(&[IValue::from(img)])?;
+        let pred_t = tch::Tensor::try_from(pred)?;
+        println!("pred_t {:?}", pred_t);
+        Ok(())
+    }
+
+    pub fn run(&self, image: &tch::Tensor, conf_thresh: f32, iou_thresh: f64) -> Result<(), Error> {
+        let mut img = tch::vision::image::resize(&image, self.w, self.h)?;
+        img = img
+            .unsqueeze(0)
+            .to_kind(tch::Kind::Float)
+            .to_device(self.device)
+            .g_div_scalar(255.0);
+        let pred = self.model.forward_is(&[IValue::from(img)]).unwrap();
         let pred_T = tch::Tensor::try_from(pred)?;
         println!("pred_t, {:?}", pred_T);
         // todo!()
         Ok(())
-    } 
+    }
 }
